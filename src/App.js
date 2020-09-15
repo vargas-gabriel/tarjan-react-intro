005ba52686ac3d87926c6a1f32e8615840806bec
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-09-15 10:19:53 -0500

    render pic gallery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import PictureGallery from './PictureGallery';
+//react component incantation 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Happy React Day!</h1>
+        <h3>Things to learn</h3>
+        <ul>
+          <li>Html inside JS files</li>
+          <li>Render</li>
+          <li>Import/Export</li>
+        </ul>
+        <h3>Check out Pics</h3>
+        <PictureGallery/>
       </div>
     );
   }
---
+++ src/PictureGallery.js
@@ -0,0 +1,19 @@
+//import react lib and component var
+import React, { Component } from 'react';
+
+
+class PictureGallery extends Component{
+    render(){
+        return (
+            <div>
+                <img src="https://picsum.photos/200/200"/>
+                <img src="https://picsum.photos/200/200"/>
+                <img src="https://picsum.photos/200/200"/>
+                <img src="https://picsum.photos/200/200"/>
+                <img src="https://picsum.photos/200/200"/>
+            </div>
+        );
+    }
+}
+
+export default PictureGallery;
